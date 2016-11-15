8a44f094b7638979494b0fdb7ce88ba554ec7fc2
VilRan <VilleMattiRantala@gmail.com>
2016-11-15 16:26:39 +0200

    Add Topic class

--- YetAnotherPostingBoard/GetMessages.php
+++ YetAnotherPostingBoard/GetMessages.php
@@ -3,6 +3,7 @@ $messages = array();
 $messages[] = array("text" => "Tdfsdfsdljfasdklfj sdfl fjasfkl fjsdlasdfjksdfl");
 $messages[] = array("text" => "kgöflgk sdg ödfklg k ögweo jfe wwef e");
 $messages[] = array("text" => "efwkf wefko wekefp epw kowefp of");
+$messages[] = array("text" => "gergwfk wpef kweop kpwefo kpf");
 
 $response = json_encode($messages);
 header('Content-type: application/json');
--- YetAnotherPostingBoard/script.jsx
+++ YetAnotherPostingBoard/script.jsx
@@ -1,18 +1,3 @@
-var MessageContainer = React.createClass({
-    render: function () {
-        "use strict";
-        return (
-            <div>
-                <div className="messageContainer">
-                    <Message text="Fefefwefefef  afa sf" />
-                    <Message text="hdfhdfghdfgdfg" />
-                    <Message text="sgrsrgrgsegergsrg" />
-                </div>
-            </div>
-        );
-    }
-});
-
 var Message = React.createClass({
     render: function () {
         "use strict";
@@ -26,7 +11,60 @@ var Message = React.createClass({
     }
 });
 
+var MessageContainer = React.createClass({                                         
+    render: function () {
+        "use strict";
+        var messages = this.props.data.map( function(value, index) {
+            return (
+                <Message text={value.text} />
+            );
+        });
+
+        return (
+            <div>
+                {messages}
+            </div>
+        );
+    }
+});
+
+
+var Topic = React.createClass({
+    getInitialState: function() {
+        return {data: []};
+    },
+    
+    getMessages: function() {
+        $.ajax({
+            url: "GetMessages.php",
+            dataType: "json",
+            cache: false,
+            success: function(data) {
+                this.setState({
+                    data: data
+                });
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error("ERROR: getMessages: ", status, err.toString());
+            }
+        });
+    },
+    
+    componentDidMount: function() {
+        this.getMessages();
+        //setInterval(this.getMessages, 5000);
+    },
+    
+    render: function() {
+        return (
+            <div className="topic">
+                <MessageContainer data={this.state.data} />
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
-    <MessageContainer />,
+    <Topic />,
     document.getElementById("messages")
 );
