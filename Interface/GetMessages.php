810e5700e57850417e84a4353757e56d05c6a97f
VilRan <VilleMattiRantala@gmail.com>
2016-11-16 16:50:55 +0200

    Add Database.class.php

--- Interface/GetMessages.php
+++ Interface/GetMessages.php
@@ -1,34 +1,6 @@
 <?php
-require_once("PathInit.php");
-require_once(DB_INIT_PATH);
-$db = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8", DB_USER, DB_PASSWORD);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
-function getAvatar($db, $userID)
-{
-    $statement = $db->query("SELECT AvatarUrl FROM User WHERE Id=$userID");
-    $avatar = "missing.png";
-    while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-    {
-        $avatar = $row["AvatarUrl"];
-    }
-    return $avatar;
-}
-
-$messages = array();
-$statement = $db->query('SELECT * FROM Message');
-while ($row = $statement->fetch(PDO::FETCH_ASSOC)) 
-{
-    $userId = $row["UserId"];
-    $avatar = getAvatar($db, $userId);
-    
-    $messages[] = array (
-        "avatar" => $avatar,
-        "text" => $row["Text"]
-    );
-}
-
-$response = json_encode($messages);
+require_once("Database.class.php");
+$db = new Database();
+$response = json_encode($db->getMessages(1));
 header('Content-type: application/json');
 echo $response;
