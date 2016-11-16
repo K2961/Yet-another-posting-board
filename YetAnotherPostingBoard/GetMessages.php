c14bc3d237d925c01567da7b73a012b856f51045
K2961 <k2961@student.jamk.fi>
2016-11-16 15:18:46 +0200

    Modify style

--- YetAnotherPostingBoard/GetMessages.php
+++ YetAnotherPostingBoard/GetMessages.php
@@ -1,9 +1,9 @@
 <?php
 $messages = array();
-$messages[] = array("text" => "Tdfsdfsdljfasdklfj sdfl fjasfkl fjsdlasdfjksdfl");
-$messages[] = array("text" => "kgöflgk sdg ödfklg k ögweo jfe wwef e");
-$messages[] = array("text" => "efwkf wefko wekefp epw kowefp of");
-$messages[] = array("text" => "gergwfk wpef kweop kpwefo kpf");
+$messages[] = array("text" => "Tdfsdfsdljfasdklfj sdfl fjasfkl fjsdlasdfjksdfl", "avatar" => "kuva/avatarph.png");
+$messages[] = array("text" => "kgöflgk sdg ödfklg k ögweo jfe wwef e", "avatar" => "kuva/avatarph.png");
+$messages[] = array("text" => "efwkf wefko wekefp epw kowefp of", "avatar" => "kuva/avatarph.png");
+$messages[] = array("text" => "gergwfk wpef kweop kpwefo kpf", "avatar" => "kuva/avatarph.png");
 
 $response = json_encode($messages);
 header('Content-type: application/json');
