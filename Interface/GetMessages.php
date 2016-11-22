1bc87746027c60698dd4f185700944c231708eaa
VilRan <VilleMattiRantala@gmail.com>
2016-11-22 17:50:30 +0200

    Unify style

--- Interface/GetMessages.php
+++ Interface/GetMessages.php
@@ -3,5 +3,5 @@ require_once("Database.class.php");
 $database = new Database();
 $messages = $database->getMessages(1);
 $response = json_encode($messages);
-header('Content-type: application/json');
+header("Content-type: application/json");
 echo $response;
--- Interface/GetTopic.php
+++ Interface/GetTopic.php
@@ -2,5 +2,5 @@
 require_once("Database.class.php");
 $database = new Database();
 $response = json_encode($database->getTopic(1));
-header('Content-type: application/json');
+header("Content-type: application/json");
 echo $response;
