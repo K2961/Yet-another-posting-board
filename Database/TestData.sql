a4c8a798857a0b2cdb26b095ccbdd07cd3079d1e
Ville Matti Rantala <VilleMattiRantala@gmail.com>
2016-12-07 23:33:44 +0200

    Remove TestData.sql

--- Database/TestData.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO User (Id, Name, Password, AvatarUrl, Joined)
-VALUES (1, "Test User", "test", "test.png", NOW());
-
-INSERT INTO Topic (Id, UserId, ParentId, Title, Posted)
-VALUES (1, 1, NULL, "Test Topic", NOW());
-
-INSERT INTO Message(Id, TopicId, UserId, Text, Posted)
-VALUES (1, 1, 1, "Test text", NOW());
